d3ed7a6893c288efe6b9ac19827b6142cd3b73d7
meraygrey <alexaD74@mail.ru>
2021-06-18 13:45:32 +0500

    2

--- src/main/java/ru/iu3/fclient/MainActivity.java
+++ src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,17 +1,23 @@
 package ru.iu3.fclient;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 
-import ru.iu3.fclient.databinding.ActivityMainBinding;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,11 +33,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        byte[] rnd = randomBytes(16);
+        Button btn = findViewById(R.id.btnClickMe);
+        btn.setOnClickListener(this::onButtonClick);
+
+
+        int res = initRng();
+        Log.i("fclient", "Init Rng = " + res);
+        byte[] v = randomBytes(10);
+
+
+        /*byte[] rnd = randomBytes(16);
+        byte[] data = {1,2,3,4,5,6,7,8};
+        byte[] encText = encrypt(rnd, data);
+        byte[] decText = decrypt(rnd, encText);*/
+
+
 
         // Example of a call to a native method
-        // TextView tv = findViewById(R.id.sample_text);
-        // tv.setText(stringFromJNI());
+        /*TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());*/
+
     }
 
     public static byte[] StringToHex(String s)
@@ -52,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     {
         //A.
         //Log.i("fapptag", "Clicked");
-        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
         //B.
         byte[] key = StringToHex("0123456789ABCDEF0123456789ABCDE0");
         byte[] enc = encrypt(key, StringToHex("000000000000000102"));
@@ -64,8 +85,9 @@ public class MainActivity extends AppCompatActivity {
         //startActivity(it);
         //D.
         Intent it = new Intent(this, PinpadActivity.class);
+        //noinspection deprecation
         startActivityForResult(it, 0);
-        //TestHttpClient();
+        TestHttpClient();
     }
 
     @Override
@@ -84,12 +106,47 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    protected void TestHttpClient()
+    {
+        new Thread(()->{
+            try {
+                HttpURLConnection uc = (HttpURLConnection) (new URL("http://10.0.2.2:8081/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(()-> Toast.makeText(this, title, Toast.LENGTH_SHORT).show());
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+
+    protected String getPageTitle(String html)
+    {
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find())
+            p = matcher.group(1);
+        else
+            p = "Not found";
+        return p;
+        /*int pos = html.indexOf("<title");
+        String p = "not found";
+        if (pos >= 0)
+        {
+            int pos2 = html.indexOf("<", pos + 1);
+            if (pos >= 0)
+                p = html.substring(pos + 7, pos2);
+        }
+        return p;*/
+    }
+
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     //public native String stringFromJNI();
-
     public static native int initRng();
     public static native byte[] randomBytes(int no);
     public static native byte[] encrypt(byte[] key, byte[] data);
